0b6733813b2d996d9efee1281cd46a13f7b78325
Subhag Oak <soak@microsoft.com>
2016-01-12 10:10:36 -0800

    Adding protractor file

--- tests/protractor.config.js
+++ tests/protractor.config.js
@@ -1,21 +1,32 @@
 exports.config = {
     framework: 'jasmine2',
+    seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
+    specs: ['e2e/*.tests.js'],
     capabilities: {
-            // You can use other browsers
-            // like firefox, phantoms, safari, IE (-_-)
-            'browserName': 'chrome' 
+        browserName: '',
+        appiumVersion: '1.4.11',
+        deviceName: 'Google Nexus 7 HD Emulator',
+        deviceOrientation: 'portrait',
+        platformName: 'Android',
+        app: 'sauce-storage:android-debug.apk',
+        autoWebview: true,
+        chromeOptions: {
+            androidActivity: "com.ionicframework.myapp.MainActivity",
+            androidPackage: "com.ionicframework.myapp"
+        }
     },
-    specs: [
-            // We are going to make this file in a minute
-        'e2e/*.tests.js'
-    ],
+    onPrepare: function() {
+        var jasmineReporters = require('jasmine-reporters');
+        var junitReporter = new jasmineReporters.JUnitXmlReporter({
+            savePath: 'bin/test',
+            consolidateAll: false,
+            filePrefix: 'TESTS-E2E-'
+        });
+        jasmine.getEnv().addReporter(junitReporter);
+    },
+
     jasmineNodeOpts: {
         showColors: true,
-        defaultTimeoutInterval: 30000,
-        isVerbose: true,
-    },
-    allScriptsTimeout: 20000,
-    onPrepare: function(){
-        browser.driver.get('http://localhost:8100');
+        defaultTimeoutInterval: 30000
     }
-};
+}
---
+++ tests/protractorlocal.config.js
@@ -0,0 +1,21 @@
+exports.config = {
+    framework: 'jasmine2',
+    capabilities: {
+            // You can use other browsers
+            // like firefox, phantoms, safari, IE (-_-)
+            'browserName': 'chrome' 
+    },
+    specs: [
+            // We are going to make this file in a minute
+        'e2e/*.tests.js'
+    ],
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        isVerbose: true,
+    },
+    allScriptsTimeout: 20000,
+    onPrepare: function(){
+        browser.driver.get('http://localhost:8100');
+    }
+};
